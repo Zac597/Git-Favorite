fbaa004863f472fd22438adc452deadfaf0ef5ca
Isaac <isaaclima597@gmail.com>
2023-06-27 05:01:28 -0300

    Finalização git fav

--- js/favorites.js
+++ js/favorites.js
@@ -60,14 +60,25 @@ export class FavoritesView extends Favorites{
 
   onadd() {
     const addButton = this.root.querySelector('.search button')
-    addButton.onclick = () => {
-      const {value} = this.root.querySelector('.search input')
 
-      this.add(value)
+    // favoritar através da tecla Enter
+    window.document.onkeyup = event => {
+        if(event.key === "Enter"){ 
+          const { value } = this.root.querySelector('.search input')
+          this.add(value)
+        }
     }
-  }
+
+    addButton.onclick = () => {
+        const { value } = this.root.querySelector('.search input')
+
+        this.add(value)
+    }
+}
 
   update() {
+    this.emptyState()
+
     this.removeAllTr()
     
     this.entries.forEach( user => {
@@ -97,20 +108,20 @@ export class FavoritesView extends Favorites{
 
   tr.innerHTML =  `
       <td class="user">
-      <img src="https://github.com/Zac597.png" alt="imagem de user">
-      <a href="https://github.com/Zac597" target="_blank">
-        <p>Isaac Lima</p>
-        <span>/Zac597</span>
+      <img src="" alt="">
+      <a href="" target="_blank">
+        <p></p>
+        <span></span>
       </a>
     </td>
-    <td class="repositories">
-      100
-    </td>
-    <td class="followers">
-      200
-    </td>
+    <td class="repositories"></td>
+    <td class="followers"></td>
     <td>
-      <button class="remove">Remover</button>
+      <button class="remove">
+        <svg fill="#ff0000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
+        <path d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z"/>
+        </svg>
+      </button>
     </td>
       `
 
@@ -126,4 +137,13 @@ export class FavoritesView extends Favorites{
     })
   }
 
+
+
+emptyState() {
+    if (this.entries.length === 0) {
+        this.root.querySelector('.empty-state').classList.remove('hide')
+    } else {
+        this.root.querySelector('.empty-state').classList.add('hide')
+    }
+  }
 }
